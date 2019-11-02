a8661fc9ae848595aecdb9ae751f5d0ef352eed2
Alexei Andrushievich <vint21h@vint21h.pp.ua>
2019-11-02 17:42:09 +0200

    Add bandit to checks (with codacy compat config)

--- tests/settings.py
+++ tests/settings.py
@@ -5,6 +5,7 @@
 
 
 import pathlib
+import random
 import sys
 from typing import Dict, List, Union  # pylint: disable=W0611
 
@@ -13,7 +14,12 @@ from typing import Dict, List, Union  # pylint: disable=W0611
 sys.path.insert(0, str(pathlib.Path(__file__).absolute().parent.parent.parent))
 
 # secret key
-SECRET_KEY = "django-read-only-admin-test-key"  # type: str
+SECRET_KEY = "".join(
+    [
+        random.choice("abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)")  # nosec
+        for i in range(50)
+    ]
+)  # type: str
 
 # configure databases
 DATABASES = {
